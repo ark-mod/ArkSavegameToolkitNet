35cb51f9a85a75cc0ef9b141c997dc8d6f088271
jefferson-1 <jefferson@jeffersonnunnconsulting.com>
2018-11-20 09:47:41 -0600

    Bump build

--- ArkSavegameToolkitNet.Domain/Properties/AssemblyInfo.cs
+++ ArkSavegameToolkitNet.Domain/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ArkSavegameToolkitNet.Domain")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.0.1")]
-[assembly: AssemblyFileVersion("1.9.0.1")]
+[assembly: AssemblyVersion("1.9.0.2")]
+[assembly: AssemblyFileVersion("1.9.0.2")]
 
 //todo: temp for dev purposes
 [assembly: InternalsVisibleTo("ArkSavegameToolkitNet.TestConsoleApp")]
