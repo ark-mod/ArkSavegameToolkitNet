3a76cc09fb71f2c9f66777d6e1e0e5422a13f6b2
jefferson-1 <jefferson@jeffersonnunnconsulting.com>
2020-01-04 08:46:52 -0600

    Bump Build

--- ArkSavegameToolkitNet.Domain/Properties/AssemblyInfo.cs
+++ ArkSavegameToolkitNet.Domain/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ArkSavegameToolkitNet.Domain")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
